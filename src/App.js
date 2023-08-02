7d12422146a2aa7202cafbf0249fa32ad5cb6328
Nuraddin <93943326+NuraddinGit@users.noreply.github.com>
2023-08-02 18:12:04 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { convertArabic } from 'arabic-reshaper'
 
 function App() {
+  const [inputText, setInputText] = useState('')
+  const [outputText, setOutputText] = useState('')
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value)
+  }
+
+  const handleButtonClick = () => {
+    const reshapedText = convertArabic(inputText).split('').reverse().join('')
+    setOutputText(reshapedText)
+  }
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleButtonClick()
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Arabic Reshaper App</h1>
+      <input
+        type="text"
+        value={inputText}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
+        placeholder="Enter Arabic text..."
+      />
+      <button onClick={handleButtonClick}>Reshape</button>
+      <div>
+        <h2>Output Text:</h2>
+        <p>{outputText}</p>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
